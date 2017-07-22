9d957a4a81e14995764f0cf1ee71faf49adcd29c
Hakob <hakoaraqelyan@gmail.com>
2017-07-22 14:58:56 +0400

    Update settings.py

--- projektr/settings.py
+++ projektr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!)atyir=ears_76o8zy^8oz1(=w7*_qs%9h1sx^n$fk94)qx5$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
